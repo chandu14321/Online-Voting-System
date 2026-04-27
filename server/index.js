2ed6be29e28be74b47ea4a99226f7314c3f533fe
chandu14321 <63911604+chandu14321@users.noreply.github.com>
2026-04-27 23:30:04 +0530

    changed the evn-database to MONGO_URI

--- server/index.js
+++ server/index.js
@@ -8,12 +8,13 @@ const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
 
 // Connect to MongoDB
-mongoose.connect(process.env.DATABASE, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.error('Mongo error:', err));
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('Mongo error:', err));
 
 const routes = require('./routes');
 const handle = require('./handlers');
--- server/models/index.js
+++ server/models/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.set('debug', true);
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.DATABASE);
+mongoose.connect(process.env.MONGO_URI);
 
 module.exports.User = require('./user');
 module.exports.Poll = require('./poll');
